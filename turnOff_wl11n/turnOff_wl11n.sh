57bfbf0d73c5964e875e4fe00d030a4bfee10193
shengpo <owen.s096@gmail.com>
2013-01-15 23:07:04 +0800

    change #!/bin/sh to #!/bin/bash

--- turnOff_wl11n/turnOff_wl11n.sh
+++ turnOff_wl11n/turnOff_wl11n.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 
 #This script is to solve slow wireless connection problem on Linux. (only happened sometimes)
@@ -9,7 +9,7 @@ kernel_release=$(uname -r)
 echo ""
 echo "your kernel is $kernel_release"
 
-if [ "$kernel_release" = "3.7.1-030701-generic" ]; then
+if [ "$kernel_release" == "3.7.1-030701-generic" ]; then
 	echo "rmmod iwldvm & iwlwifi"
 	echo "modprobe iwlwifi & disable 11n"
 	sudo rmmod iwldvm && sudo rmmod iwlwifi && sudo modprobe iwlwifi 11n_disable=1
